a39333a850f9cffdffc0464d84776067f60eaabd
Mislav Franjo <mislav.franja@gmail.com>
2016-11-03 19:39:59 +0100

    destruktor bez tocke

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -18,8 +18,8 @@ namespace Vsite.CSharp
         // Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
         ~KlasaSDestruktorom()
         {
-            Console.WriteLine($"Destruktor objekta br. {RedniBroj}.");
-            Debug.WriteLine($"Destruktor objekta br. {RedniBroj}.");
+            Console.WriteLine($"Destruktor objekta br. {RedniBroj}");
+            Debug.WriteLine($"Destruktor objekta br. {RedniBroj}");
 
         }
 
